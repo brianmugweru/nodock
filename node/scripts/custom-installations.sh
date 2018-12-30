4dc4316a2a997b9906532078e7aec52e8258a169
Brian Mugweru <brian@ralphowino.com>
2018-12-30 03:02:18 +0300

    upate node dockerfile

---
+++ node/scripts/custom-installations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $NODE_TEST_ENVIRONMENT_MOCHA = true ]]; then
+    npm i -g mocha;
+fi
+
+if [[ $NODE_USE_NPM = true ]]; then
+    npm i -g npm@latest;
+fi
+
+if [[ $NODE_USE_YARN = true ]]; then 
+    npm i -g yarn;
+fi
+
+if [[ $NODE_ISTANBUL_TEST_COVERAGE = true ]]; then
+    npm i -g istanbul;
+fi
+
+if [[ $NODE_INSTALL_IMAGE_MAGICK = true ]]; then
+    apt-get install -y imagemagick;
+fi
+
